e5ae073580fcfdffd9f5795dd0b922f8d6f77034
azzrf <fairuzizuan019@gmail.com>
2024-03-23 21:38:05 +0700

    update dashboard posts

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -1,29 +1,30 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-8">
-                <h2>{{ $post->title }}</h2>
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <h2>{{ $post->title }}</h2>
 
-                <a href="/dashboard/posts" class="btn btn-success">
-                    <i class="bi bi-arrow-left"></i>Back to All Posts
-                </a>
-                <a href="" class="btn btn-warning">
-                    <i class="bi bi-pencil"></i>Edit
-                </a>
-                <a href="" class="btn btn-danger">
-                    <i class="bi bi-x-circle"></i>Delete
-                </a>
+                    <a href="/dashboard/posts" class="btn btn-success">
+                        <i class="bi bi-arrow-left"></i>Back to All Posts
+                    </a>
+                    <a href="" class="btn btn-warning">
+                        <i class="bi bi-pencil"></i>Edit
+                    </a>
+                    <a href="" class="btn btn-danger">
+                        <i class="bi bi-x-circle"></i>Delete
+                    </a>
 
+                    <img src="https://source.unsplash.com/1200x500/?{{ $post->category->nama }}"
+                        alt="{{ $post->category->nama }}" class="img-fluid mt-3" />
 
-                <img src="https://source.unsplash.com/1200x500/?{{ $post->category->nama }}"
-                    alt="{{ $post->category->nama }}" class="img-fluid mt-3" />
-
-                <article class="my-3 fs-6">
-                    {!! $post->body !!}
-                </article>
+                    <article class="my-3 fs-6">
+                        {!! $post->body !!}
+                    </article>
+                </div>
             </div>
         </div>
-    </div>
+    </main>
 @endsection
